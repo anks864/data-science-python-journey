d94a3e8e337531c81af4457f1380d8514f348560
Ankita Roy <roy864ankita@gmail.com>
2026-04-18 20:42:49 +0530

    Vector visualization

--- 05_linear_algebra/no17_vector_operations.py
+++ 05_linear_algebra/no17_vector_operations.py
@@ -14,5 +14,25 @@ import seaborn as sns
 
 
 # ==============================
+# Basic Vector Visualization
+# ==============================
+plt.quiver(0, 0, 4, 5)
+plt.show()
+
+
+# ==============================
+# Multiple Vector Plot
+# ==============================
+plt.quiver(0, 0, 2, 3, scale_units='xy', angles='xy', scale='1', color='g')
+plt.quiver(0, 0, 3, -2, scale_units='xy', angles='xy', scale='1', color='r')
+plt.quiver(0, 0, 5, 1, scale_units='xy', angles='xy', scale='1', color='y')
+plt.quiver(0, 0, -1, 5, scale_units='xy', angles='xy', scale='1', color='y')
+
+plt.xlim(-8, 8)
+plt.ylom(-8, 8)
+
+plt.show()
+
+# ==============================
 # EOF: Feel free to open an issue to report a bug or discrepancy
 # ==============================
