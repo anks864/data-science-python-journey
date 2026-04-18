fc0223a3b8e39d6cf91297473e704eb7b0fa5357
Ankita Roy <roy864ankita@gmail.com>
2026-04-18 20:58:29 +0530

    Create no18_matrix_and_operations.py

---
+++ 05_linear_algebra/no18_matrix_and_operations.py
@@ -0,0 +1,10 @@
+# ==============================
+# File: no18_matrix_and_operations.py
+# Topic: Matrix and Operations
+# Description: Covers matrix creation and basic operations using NumPy
+# ==============================
+
+
+# ==============================
+# EOF: Feel free to open an issue to report a bug or discrepancy
+# ==============================
