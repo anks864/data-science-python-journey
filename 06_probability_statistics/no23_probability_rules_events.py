fe81dde50290e0a7ff230b014a9bcb910432436a
Ankita Roy <roy864ankita@gmail.com>
2026-05-02 23:27:04 +0530

    Update no23_probability_rules_events.py

--- 06_probability_statistics/no23_probability_rules_events.py
+++ 06_probability_statistics/no23_probability_rules_events.py
@@ -23,6 +23,21 @@ def complement(p_a):
 # ==============================
 # Event Relationships
 # ==============================
+def union(p_a, p_b, p_a_and_b):
+    return p_a + p_b - p_a_and_b
+
+def intersection_independent(p_a, p_b):
+    return p_a * p_b
+
+
+# ==============================
+# Types of Events
+# ==============================
+def are_mutually_exclusive(p_a_and_b):
+    return p_a_and_b == 0
+
+def are_independent(p_a, p_b, p_a_and_b):
+    return abs(p_a * p_b - p_a_and_b) < 1e-6
 #==============================================================#
 # EOF: Feel free to open an issue to report a bug or discrepancy
 #==============================================================#
