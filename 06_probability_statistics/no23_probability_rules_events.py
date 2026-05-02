b90587de7e1919e6486c9af76a442273be612ad3
Ankita Roy <roy864ankita@gmail.com>
2026-05-02 23:25:17 +0530

    Create no23_probability_rules_events.py

---
+++ 06_probability_statistics/no23_probability_rules_events.py
@@ -0,0 +1,16 @@
+"""
+File: no23_probability_rules_events.py
+Topic: Probability Rules and Types of Events
+Description: Covers probability axioms, event types, and fundamental probability rules.
+"""
+
+# ==============================
+# Importing Libraries
+# ==============================
+
+import itertools
+
+
+#==============================================================#
+# EOF: Feel free to open an issue to report a bug or discrepancy
+#==============================================================#
