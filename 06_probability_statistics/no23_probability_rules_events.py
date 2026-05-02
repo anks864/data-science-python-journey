d26c5ff15000ab64a4f7162b350d51856c14fb74
Ankita Roy <roy864ankita@gmail.com>
2026-05-02 23:26:12 +0530

    Update no23_probability_rules_events.py

--- 06_probability_statistics/no23_probability_rules_events.py
+++ 06_probability_statistics/no23_probability_rules_events.py
@@ -7,10 +7,22 @@ Description: Covers probability axioms, event types, and fundamental probability
 # ==============================
 # Importing Libraries
 # ==============================
-
 import itertools
 
 
+# ==============================
+# Basic Probability Rules
+# ==============================
+def probability(event_outcomes, total_outcomes):
+    return event_outcomes / total_outcomes
+
+def complement(p_a):
+    return 1 - p_a
+
+
+# ==============================
+# Event Relationships
+# ==============================
 #==============================================================#
 # EOF: Feel free to open an issue to report a bug or discrepancy
 #==============================================================#
