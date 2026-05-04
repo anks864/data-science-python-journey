80bd62c86003a290bcefc04e447621eb7825aa75
Ankita Roy <roy864ankita@gmail.com>
2026-05-04 22:20:00 +0530

    Create no25_random_variables.py

---
+++ 06_probability_statistics/no25_random_variables.py
@@ -0,0 +1,16 @@
+"""
+File: no25_random_variables.py
+Topic: Random Variables
+Description: Covers discrete and continuous random variables, expectation, and variance.
+"""
+
+# ==============================
+# Importing Libraries
+# ==============================
+
+import numpy as np
+
+
+#============================================================#
+# EOF
+#============================================================#
