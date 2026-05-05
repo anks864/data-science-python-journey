8e4db476bbf5cb0a1b310812fa7327bf84b4516c
Ankita Roy <roy864ankita@gmail.com>
2026-05-05 23:40:27 +0530

    Create no26_advanced_distributions.py

---
+++ 06_probability_statistics/no26_advanced_distributions.py
@@ -0,0 +1,10 @@
+"""
+File: no26_advanced_distributions.py
+Topic: Probability Distributions (Advanced)
+Description: Covers Poisson, Exponential, and Uniform distributions.
+"""
+
+
+#==============================================================#
+# EOF: Feel free to open an issue to report a bug or discrepancy
+#==============================================================#
