5dda0d224cd571a06c67d74958aecfa01a80af5f
Ankita Roy <roy864ankita@gmail.com>
2026-05-06 22:35:57 +0530

    Create no27_sampling_and_estimations.py

---
+++ 06_probability_statistics/no27_sampling_and_estimations.py
@@ -0,0 +1,10 @@
+"""
+File: no27_sampling_and_estimations.py
+Topic: Sampling and Parameter Estimation
+Description: Covers sampling techniques and estimation methods like MLE.
+"""
+
+
+#==============================================================#
+# EOF: Feel free to open an issue to report a bug or discrepancy
+#==============================================================#
