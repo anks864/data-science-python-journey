72b51dedef715edaeed54e975d30f338556a9acc
Ankita Roy <roy864ankita@gmail.com>
2026-05-06 22:37:14 +0530

    Import libraries + Sampling techniques functions

--- 06_probability_statistics/no27_sampling_and_estimations.py
+++ 06_probability_statistics/no27_sampling_and_estimations.py
@@ -5,6 +5,19 @@ Description: Covers sampling techniques and estimation methods like MLE.
 """
 
 
+# ==============================
+# Importing Libraries
+# ==============================
+import numpy as np
+
+
+# ==============================
+# Sampling Techniques
+# ==============================
+def simple_random_sample(data, size):
+    return np.random.choice(data, size=size, replace=False)
+
+
 #==============================================================#
 # EOF: Feel free to open an issue to report a bug or discrepancy
 #==============================================================#
