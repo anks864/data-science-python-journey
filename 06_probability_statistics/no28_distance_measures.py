cf509c757a244113bf0ccb29afe5107e2b91ad06
Ankita Roy <roy864ankita@gmail.com>
2026-05-07 15:21:32 +0530

    Create no28_distance_measures.py

---
+++ 06_probability_statistics/no28_distance_measures.py
@@ -0,0 +1,11 @@
+"""
+File: no28_distance_measures.py
+Topic: Measures of Distance
+Description: Covers Euclidean, Manhattan, and Cosine distance used in ML.
+"""
+
+
+
+#============================================================#
+# EOF
+#============================================================#
