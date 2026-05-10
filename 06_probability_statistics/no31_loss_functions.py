98b4e775e534e152bd8fbb38d5157f8938dcee12
Ankita Roy <roy864ankita@gmail.com>
2026-05-10 23:26:36 +0530

    Create no31_loss_functions.py

---
+++ 06_probability_statistics/no31_loss_functions.py
@@ -0,0 +1,10 @@
+"""
+File: no31_loss_functions.py
+Topic: Loss Functions
+Description: Covers MSE and MAE used in regression.
+"""
+
+
+#==============================================================#
+# EOF: Feel free to open an issue to report a bug or discrepancy
+#==============================================================# 
