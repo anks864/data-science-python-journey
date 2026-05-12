b7bbce8a32d143a7a35c64bb62472a4338723121
Ankita Roy <roy864ankita@gmail.com>
2026-05-12 22:52:45 +0530

    Add assumptions check function

--- 06_probability_statistics/no32_regression_assumptions.py
+++ 06_probability_statistics/no32_regression_assumptions.py
@@ -18,6 +18,16 @@ def residuals(y_true, y_pred):
     return y_true - y_pred
 
 
+# ==============================
+# Assumption Checks
+# ==============================
+def check_mean_zero(res):
+    return np.isclose(np.mean(res), 0, atol=1e-2)
+
+def check_variance(res):
+    return np.var(res)
+
+
 #==============================================================#
 # EOF: Feel free to open an issue to report a bug or discrepancy
 #==============================================================#
