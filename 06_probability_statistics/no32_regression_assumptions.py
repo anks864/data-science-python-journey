3a6687ee3048b57b941e4c8564240fb8867000b4
Ankita Roy <roy864ankita@gmail.com>
2026-05-12 22:50:09 +0530

    Create no32_regression_assumptions.py

---
+++ 06_probability_statistics/no32_regression_assumptions.py
@@ -0,0 +1,10 @@
+"""
+File: no32_regression_assumptions.py
+Topic: Regression Assumptions
+Description: Checks basic assumptions like linearity and residual distribution.
+"""
+
+
+#==============================================================#
+# EOF: Feel free to open an issue to report a bug or discrepancy
+#==============================================================#
