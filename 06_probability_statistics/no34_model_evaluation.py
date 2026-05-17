12f63b736e086299e625b35ffc763c67c02757c2
Ankita Roy <roy864ankita@gmail.com>
2026-05-17 20:57:34 +0530

    Create no34_model_evaluation.py

---
+++ 06_probability_statistics/no34_model_evaluation.py
@@ -0,0 +1,10 @@
+"""
+File: no34_model_evaluation.py
+Topic: Model Evaluation Metrics
+Description: Covers evaluation metrics for regression models.
+"""
+
+
+#==============================================================#
+# EOF: Feel free to open an issue to report a bug or discrepancy
+#==============================================================#
