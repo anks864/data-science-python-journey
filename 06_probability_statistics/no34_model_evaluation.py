44e9386cfdd455d5546f66ddc4e7d0db04f741f1
Ankita Roy <roy864ankita@gmail.com>
2026-05-17 20:59:45 +0530

    Add function on evaluation metric - mse

--- 06_probability_statistics/no34_model_evaluation.py
+++ 06_probability_statistics/no34_model_evaluation.py
@@ -11,6 +11,13 @@ Description: Covers evaluation metrics for regression models.
 import numpy as np
 
 
+# ==============================
+# Evaluation Metrics
+# ==============================
+def mse(y_true, y_pred):
+    return np.mean((y_true - y_pred) ** 2)
+
+
 #==============================================================#
 # EOF: Feel free to open an issue to report a bug or discrepancy
 #==============================================================#
