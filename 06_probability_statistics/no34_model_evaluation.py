213eef5488050c53bfb05b37b79a81a4885f808f
Ankita Roy <roy864ankita@gmail.com>
2026-05-17 21:26:13 +0530

    Add function on evaluation metric - r2_score

--- 06_probability_statistics/no34_model_evaluation.py
+++ 06_probability_statistics/no34_model_evaluation.py
@@ -22,6 +22,13 @@ def rmse(y_true, y_pred):
 
 def mae(y_true, y_pred):
     return np.mean(np.abs(y_true - y_pred))
+
+def r2_score(y_true, y_pred):
+    ss_total = np.sum((y_true - np.mean(y_true)) ** 2)
+    ss_res = np.sum((y_true - y_pred) ** 2)
+    return 1 - (ss_res / ss_total)
+
+
 #==============================================================#
 # EOF: Feel free to open an issue to report a bug or discrepancy
 #==============================================================#
